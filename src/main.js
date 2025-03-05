46f1896af407e89cf19f8789dd49b27de0af8cbb
AidamZzzZ <adrian_alliskair@hotmail.com>
2025-03-05 10:30:05 -0400

    last commit

--- src/main.js
+++ src/main.js
@@ -1,24 +1,70 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import dayjs from "dayjs"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const currentTimeEl = document.querySelector(".current-time");
+const currentDateEl = document.querySelector(".current-date");
 
-setupCounter(document.querySelector('#counter'))
+const now = dayjs();
+
+let day = now.date()
+let month = now.month() + 1
+let year = now.year()
+
+function formatDateTime(day, month, year) {
+  let dateDa;
+  let dateMonth;
+
+  if (day === 1) {
+    dateDa = "Monday";
+  } else if (day === 2) {
+    dateDa = "Tuesday";
+  } else if (day === 3) {
+    dateDa = "Wednesday";
+  } else if (day === 4) {
+    dateDa = "Thursday";
+  } else if (day === 5) {
+    dateDa = "Friday";
+  } else if (day === 6) {
+    dateDa = "Saturday";
+  } else if (day === 7) {
+    dateDa = "Sunday";
+  };
+
+  if (month === 1) {
+    dateMonth = "January";
+  } else if (month === 2) {
+    dateMonth = "February";
+  } else if (month === 3) {
+    dateMonth = "March";
+  } else if (month === 4) {
+    dateMonth = "April";
+  } else if (month === 5) {
+    dateMonth = "May";
+  } else if (month === 6) {
+    dateMonth = "June";
+  } else if (month === 7) {
+    dateMonth = "July";
+  } else if (month === 8) {
+    dateMonth = "August";
+  } else if (month === 9) {
+    dateMonth = "September";
+  } else if (month === 10) {
+    dateMonth = "October";
+  } else if (month === 11) {
+    dateMonth = "November";
+  } else if (month === 12) {
+    dateMonth = "December";
+  }
+
+  return `${dateDa}, ${day} ${dateMonth}, ${year}`;
+};
+
+
+function showDate() {
+  let formatDate = formatDateTime(day, month, year)
+  let hour = now.format("HH:mm:ss");
+
+  currentTimeEl.innerText = hour;
+  currentDateEl.innerText = formatDate;
+}
+
+setInterval(showDate, 1000)
